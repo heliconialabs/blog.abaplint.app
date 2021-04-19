4d28be485b12dac0131a8740897bf9ff227ce063
larshp <larshp@hotmail.com>
2021-04-19 13:27:07 +0200

    upd

--- src/footer.ts
+++ src/footer.ts
@@ -4,7 +4,7 @@ export function footer() {
   <footer class="container text-center">
     <hr>
     <small class="mb-3 text-muted">
-    © 2020 <a class="link-secondary" href="https://heliconialabs.com">Heliconia Labs</a><br>
+    © <a class="link-secondary" href="https://heliconialabs.com">Heliconia Labs</a><br>
     </small>
   </footer>
   </body>
