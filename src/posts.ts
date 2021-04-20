e0f4b8f6dc1a68e268cdfecfed2dd7910b13b5cf
larshp <larshp@hotmail.com>
2021-04-20 06:36:49 +0200

    upd

--- src/posts.ts
+++ src/posts.ts
@@ -9,5 +9,15 @@ posts.push({
   title: "abaplint.app available on GitHub Marketplace",
   date: "2021-04-20",
   key: "abaplint-available-on-github-marketplace",
-  body: `TBD`,
+  body: `<a href="https://abaplint.app">abaplint.app</a> is now available on <a href="https://github.com/marketplace/abaplint">GitHub Marketplace</a>
+
+  Setup #ABAP static analysis and continious integration in just a few clicks, with:
+
+  <ul>
+  <li>Large ruleset</li>
+  <li>Pull request integration</li>
+  <li>Extra code insights</li>
+  </ul>
+
+  Many more features in the pipeline :)`,
 });
